dafe0ec4d6d41fc900f884bba42cdc93677a9524
minhchientnck <44605846+minhchientnck@users.noreply.github.com>
2021-01-17 22:33:22 +0700

    Create alarm-svg.js

---
+++ alarm-svg.js
@@ -0,0 +1,14 @@
+var R = 40;
+var CX = 50;
+var CY = 50;
+var rs = [];
+for(var i = 0; i < 360; i+=6) {
+ console.log(i/6);
+ var RAD = (i * 2 * Math.PI) / 360;
+ var SIN = Math.sin(RAD);
+ var COS = Math.cos(RAD);
+ var cx = (SIN * R) + CX;
+ var cy = CY - (COS * R);
+ rs.push('<circle cx="'+cx+'" cy="'+cy+'" r="1" fill={props.color} stroke={props.color} strokeWidth={`${props.strokeWidth}`} />');
+}
+document.write(rs);
