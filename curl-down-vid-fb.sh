6a24edec7a199887a0d5d69577ffd4787e0ff155
minhchientnck <44605846+minhchientnck@users.noreply.github.com>
2021-04-26 21:33:27 +0700

    Create curl-down-vid-fb.sh

---
+++ curl-down-vid-fb.sh
@@ -0,0 +1,21 @@
+curl -o bach_hoa_xanh.mp3 \
+'https://video-sin6-1.xx.fbcdn.net/v/t42.1790-2/44040246_1922528244459809_1958899871779913728_n.mp4?_nc_cat=109&ccb=1-3&_nc_sid=5aebc0&efg=eyJ2ZW5jb2RlX3RhZyI6ImRhc2hfYXVkaW9fYWFjcF82NF9mcmFnXzJfYXVkaW8ifQ%3D%3D&_nc_ohc=R_aAnNqZuDIAX9BLRbU&_nc_ht=video-sin6-1.xx&oh=238a24c54ab2bfac4f55466481c4dc3c&oe=6086EF04&bytestart=0&byteend=10000000' \
+  -H 'authority: video-sin6-1.xx.fbcdn.net' \
+  -H 'cache-control: max-age=0' \
+  -H 'upgrade-insecure-requests: 1' \
+  -H 'user-agent: Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/94.0.202 Chrome/88.0.4324.202 Safari/537.36' \
+  -H 'accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9' \
+  -H 'sec-fetch-site: none' \
+  -H 'sec-fetch-mode: navigate' \
+  -H 'sec-fetch-user: ?1' \
+  -H 'sec-fetch-dest: document' \
+  -H 'accept-language: vi-VN,vi;q=0.9,fr-FR;q=0.8,fr;q=0.7,en-US;q=0.6,en;q=0.5' \
+  -H 'range: bytes=1492-1492' \
+  -H 'if-range: Mon, 15 Oct 2018 07:33:31 GMT' \
+  --compressed
+
+curl -o bach_hoa_xanh.mp4 \
+'https://video-sin6-2.xx.fbcdn.net/v/t42.1790-2/10000000_2185901965001735_904918578128486400_n.webm?_nc_cat=103&ccb=1-3&_nc_sid=5aebc0&efg=eyJ2ZW5jb2RlX3RhZyI6ImRhc2hfdnA5X2hhbG9fNDAwa19mcmFnXzJfdmlkZW8ifQ%3D%3D&_nc_ohc=cinxS173Sr4AX8qqHsu&_nc_ht=video-sin6-2.xx&oh=b1d80cba8ea942791bb23d844390ad42&oe=6086EC88&bytestart=0&byteend=10000000' \
+  -H 'Referer: https://www.facebook.com/' \
+  -H 'User-Agent: Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) coc_coc_browser/94.0.202 Chrome/88.0.4324.202 Safari/537.36' \
+  --compressed
